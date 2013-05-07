d2d7ec15faf61af7e836c5c1d0514f4ff0a35323
Daniel Casimir Grigg <mail@danielgrigg.com>
2013-05-08 08:06:03 +1000

    Sync with dependencies

--- src/slixrt/core.clj
+++ src/slixrt/core.clj
@@ -2,13 +2,20 @@
   (:gen-class)
   (:use (sliimp film))
   (:use slimath.core)
+  (:use (sligeom core transform bounding))
   (:use (slitrace camera shape prim core)))
   
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
+
+(def ^:dynamic *world-transform* (translate 0.0 0.2 -10))
+;(def ^:dynamic *projection* (perspective (Math/toRadians 38.) 1.0 1.0 100.))
+(def ^:dynamic *world* (instance (translate 0 0 0)
+                                (sphere 1.0)))
 
 (defn -main
-  "I don't do a whole lot."
   [& args]
-  ;; work around dangerous default behaviour in CLojure
+  ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
   (println "slixrt\n"))
 
