84b72400c2e2b0cf62dfc5e05c2b5ec22e972886
Daniel Casimir Grigg <mail@danielgrigg.com>
2013-06-10 17:46:23 +1000

    Minor demo changes

--- src/slixrt/core.clj
+++ src/slixrt/core.clj
@@ -10,7 +10,7 @@
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* true)
 
-(def ^:dynamic *world-transform* (translate 0.0 0.2 -3))
+(def ^:dynamic *world-transform* (translate 0.0 0.0 -3))
 (def ^:dynamic *projection* (perspective :fov-rads (Math/toRadians 38.) 
                                          :aspect 1.0 
                                          :near 1.0 
@@ -21,9 +21,12 @@
 (defn radiance [^Transform SP ^Sample s] 
   (let [^Ray r-camera (camera-ray (:x-film s) (:y-film s) SP)
         ^Ray r-world (transform r-camera (inverse *world-transform*))]
-    (if-let [[t p n] (trace *world* r-world) ]
+(if-let [[t p n] (trace *world* r-world) ]
       (sample s t t t)
       (sample s 0.0 0.0 0.0))))
+
+(defn radiance2 [SP s]
+  (sample s 1.0 1.0 0.0))
   
 
 (defn -main
@@ -31,11 +34,11 @@
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
 
-  (when-let [^Film F (film :bounds (rect :width 512 :height 512) 
-                      :filter (mitchell)
+  (when-let [^Film F (film :bounds (rect :width 1024 :height 1024) 
+                      :filter (gaussian)
                       :finished-f #(spit-film! % "/tmp/slixrt.exr")
                       :sampler-f stratified-seq2
-                      :samples-per-pixel 25)]
+                      :samples-per-pixel 100)]
     (println "slixrt\n")
     (let [[x0 y0 x1 y1] (rect-vec (:bounds F))
           sf (partial (:sampler-f F) (:samples-per-pixel F))
